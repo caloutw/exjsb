707100af8cbe6a6523a1b790332ddc4de9449312
caloutw <chou.addison@gmail.com>
2025-08-07 20:39:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,167 @@
+<div align="center">
+  <a href="README_EN.md">English</a> | <a href="README.md">繁體中文</a>
+</div>
+
+# ExJSB
+
+一個簡單的 JavaScript 執行沙盒，支援 Module/Commandjs。
+
+## 注意
+
+此類方法基本上是以 Module 為基礎運行，因此可能會缺少像是``__dirname``和``__filename``等commandjs預設的參數。
+
+執行時請預設保持``--experimental-vm-modules``開啟，否則會出錯。
+
+## 說明
+
+利用vm還有額外的寫法，可以讓主線程執行外部的js檔案，並且同時提供隔離模式，遇到process類型的指令，可以有效防止程式碼被惡意停止。
+
+原先想法是想說php都可以用伺服器運行某些特定代碼(例如登入)，為了提高js+http的利用性，因此我做了這個。
+
+相比原本的``import``和``require``，此方法可以同時支援兩種型態的js，並且如果出錯後，也會用非同步的方式將錯誤回報給主線程，使程序不會整個停止，因為是動態加載，所以不用像``require``和``import``每次都要做一堆動作才能用，緩存也可以被gc回收(對，我就是在說你``import``)。
+
+自身支援``import.meta.url``，因此可以使用``__dirname``。
+
+## 安裝
+
+```bash
+npm install exjsb
+```
+
+## 範例
+
+### 含main(基礎用法)
+
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 執行腳本
+exjs.execute();
+```
+
+``./script.js``
+```javascript
+//程序主入口
+export function main(){
+    console.log("Hi");
+}
+```
+---
+### 無main
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 執行腳本
+exjs.execute();
+```
+
+``./script.js``
+```javascript
+console.log("Hi");
+```
+---
+### import物件
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 執行腳本
+exjs.execute();
+```
+
+``./script.js``
+```javascript
+import fs from 'fs';
+
+console.log(fs);
+
+let os = await import("os");
+```
+---
+### require物件
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 執行腳本
+exjs.execute();
+```
+
+``./script.js``
+```javascript
+const fs = require("fs");
+
+console.log(fs);
+```
+---
+### 傳遞參數
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 執行腳本(第一個參數必為錯誤callback)
+exjs.execute((err)=>console.log(err), "Hello", "World");
+```
+
+``./script.js``
+```javascript
+export function main(A, B){
+    console.log(A, B);  //Hello World
+}
+```
+---
+### 錯誤回傳
+``./index.js``
+```javascript
+import { ExJSB } from 'exjsb';
+
+// 創建一個 ExJSB 實例
+const exjsb = new ExJSB('./script.js', true);
+
+// 有錯誤，則不會使程式整個停止
+exjs.execute((err)=>console.log(err));
+
+// 有錯誤，因為沒有callback，因此會終止整個主線程
+exjs.execute();
+```
+
+## 授權
+
+此專案採用 [MIT License](LICENSE) 授權。
+
+Copyright (c) 2024 Caloutw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,155 @@
+import vm from 'vm';
+import fs from 'fs';
+import { AsyncLocalStorage } from "node:async_hooks";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+import { createRequire } from "module";
+
+//who import me.
+const source_who = () => {
+    const source = new Error().stack.split('\n')[2].trim().match(/(file:\/\/\/[^:]*)/g)[0].replace("file:\/\/","");
+    return source;
+}
+
+//path setting.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __sourcedir = dirname(source_who());
+
+//require.
+const require = createRequire(import.meta.url);
+
+export class ExJSB {
+    /**
+     * ExJSB - A simple JS execution sandbox with "insulation" mode.
+     * 
+     * @param {string} path - Js file path.
+     * @param {boolean} [insulation=true] - If true, dangerous operations (such as process.exit, require, etc.) will be blocked.
+     * 
+     * Example:
+     *   let a = new ExJSB('process.exit(1);', true);
+     *   a.execute();
+     *   // process.exit(1) will be invalid, and the main program will NOT exit.
+     * 
+     * If insulation is false, the code can access all Node.js global objects (not recommended for untrusted code).
+     */
+    constructor(filepath, insulation = true) {
+        // Check --experimental-vm-modules is opened. Throw error.
+        const hasExperimentalVMModules = process.execArgv.includes('--experimental-vm-modules');
+        if (!hasExperimentalVMModules)
+            throw new Error('must add the --experimental-vm-modules execution parameter.');
+
+        // If path is not a string. Throw error.
+        if (typeof filepath !== 'string')
+            throw new Error('path must be a string.');
+
+        if (!fs.existsSync(path.join(__sourcedir, filepath)))
+            throw new Error('file not exist.');
+
+        //If insulation not a boolean. Throw error.
+        if (typeof insulation !== "boolean")
+            throw new Error("insulation must be a boolean.")
+
+        this.filepath = (filepath.startsWith("/")) ? filepath : path.join(__sourcedir, filepath);
+        this.insulation = insulation;
+
+        this.als = new AsyncLocalStorage();
+    }
+
+    /**
+     * execute JS code on vm.
+     * @param {Function} callback - Error callback, if missing this, will throw error.
+     * @returns {boolean} - if success return true, otherwise return false.
+     */
+    execute() {
+        //get function parm arg
+        let param = Array.from(arguments).slice(1, arguments.length);
+        //get function callback arg
+        let callback = arguments[0];
+
+        //create a error listener
+        const errorHandle = ["uncaughtException", "unhandledRejection"];
+        for (let i of errorHandle) {
+            const handler = (error) => {
+                const isVMError = this.als.getStore()?.code == "ExJSBError";
+                if (isVMError && callback) {
+                    callback(error);
+                    process.removeListener(i, handler);
+                    return;
+                }
+
+                if (!isVMError && process.listenerCount(i) > 1)
+                    return;
+
+                console.error(error);
+                process.removeListener(i, handler);
+                process.exit(1);
+                return;
+            };
+            process.on(i, handler);
+        };
+
+        let cloneGlobal = () => {
+            let cloned = Object.defineProperties(
+                { ...global },
+                Object.getOwnPropertyDescriptors(global),
+            );
+            // remove 'process' from cloneGlobal
+            if ('process' in cloned && this.insulation) {
+                delete cloned.process;
+            }
+            // add 'require'
+            cloned.require = require;
+            return cloned;
+        };
+
+        let context = vm.createContext(cloneGlobal());
+        
+        let vm_CODE = fs.readFileSync(this.filepath, "utf-8");
+
+        const sendbox = new vm.SourceTextModule(vm_CODE, {
+            context: context,
+            identifier: this.filepath,
+            initializeImportMeta(meta) { meta.url = `file://${this.filepath}`; },
+            importModuleDynamically(specifier) { return import(specifier); }
+        });
+
+        this.als.run({code: "ExJSBError"}, async () => {
+            await sendbox.link(async (specifier, referencingModule) => {
+                const target = await (async () => { try { return await import(specifier); } catch (e) { throw new Error(e); return false; } })();
+    
+                if (!target) return false;
+    
+                const module_METHOD = (() => {
+                    let support_List = [];
+    
+                    if (target.default) support_List.push("default");
+                    Object.keys(target).forEach((key) => {
+                        if (key != "default") support_List.push(key);
+                    });
+    
+                    return support_List;
+                })();
+    
+                return new vm.SyntheticModule(
+                    module_METHOD,
+                    function () { for (let method of module_METHOD) this.setExport(method, target[method]); },
+                    { context: referencingModule.context }
+                );
+            });
+    
+            await sendbox.evaluate();
+    
+            if (sendbox.namespace.main) {
+                await sendbox.namespace.main(...param);
+            }
+    
+            return true;
+        });
+    }
+}
+
+// 兼容 CommonJS 匯出
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = { ExJSB };
+}
